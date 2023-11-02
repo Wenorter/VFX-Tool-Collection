1ffe597c0d1de01d119c3a5a96edbb05e0d0837c
Kirill Stahl <13516176@student.uts.edu.au>
2023-11-02 21:28:55 +1100

    Save/Publish Tool UI Update

--- SourceCode/Pseudocode/Asset Publishing System Tool/AssetPublishingSystem.py
+++ SourceCode/Pseudocode/Asset Publishing System Tool/AssetPublishingSystem.py
@@ -1,68 +1,236 @@
-#Pseudocode for Asset Saving/Publishing Tool
+# Script Name: Save/Publish Tool for Autodesk Maya
+# Description: This tool provides a way for artists to save and publish their work in a way that
+#automates the naming of the file and where the file/s are stored.
 
+import os
+import re
 import maya.cmds as cmds
 from functools import partial
 
-def savePublishTool():
+scroll_list = None
+naming_convention = r"^[A-Z]{1,3}_([A-Z]{1}[a-z]+)+$"
+export_type = ""
+
+#=======================================          
+#----------------DEFS-------------------
+#=======================================
+
+def saveFiles():
+    save_path = "/asset_wips/saved"
+    asset_types_to_save = ["setPiece", "set", "prop", "character"]
     
-    if cmds.window('savePublishTool', exists = True):
-        cmds.deleteUI('savePublishTool')
-        
-    cmds.window('savePublishTool', resizeToFitChildren=True)
-    cmds.window('savePublishTool', widthHeight=(200, 450))
+    for asset_type in asset_types_to_save:
+        asset_group = "|" + asset_type
+        if cmds.objExists(asset_group):
+            asset_roots = cmds.listRelatives(asset_group, children=True, fullPath=True)
 
-    cmds.columnLayout(
-        backgroundColor = [0.1, 0.1, 0.1], #black skin
-        #columnAlign ="center", 
-        #columnOffset =["both", 5],
-        rowSpacing = 10
-        ) 
-    
+            for asset in asset_roots:
+                asset_name = asset.split("|")[-1]
+                export_dir = "{0}/{1}/{2}".format(save_path,asset_type,asset_name)
+                try:
+                    os.makedirs(export_dir)
+                except OSError:
+                    print(export_dir + " already exists")
+                file_name = "{0}_layout_v{1}.abc".format(asset_name, str(GetNextVersionNumber(asset_name, asset_type)).zfill(3))
+                export_file = export_dir + "/" + file_name
+                #SAVE .mb FILES HERE
 
-def saveAssets():
-    start = 0
-    end = 120
-    root = "-root pSphere1 -root pCube1" #will use command for extracting model, layout, animation, lighting
-    path_name = "C:/saveFilePath/"
-    file_name = "alembicTest"
-    extension = ".abc"  
+def publishFiles(export_type):
+    publish_path = "/asset_final/published"
+    asset_types_to_publish = ["setPiece", "set", "layout", "animation"] 
 
-    command = "-frameRange " + start + " " + end +" -uvWrite -worldSpace " + root + " -file " + path_name + file_name
-    cmds.AbcExport (j = command)
+    for asset_type in asset_types_to_publish:
+        asset_group = "|" + asset_type
+        if cmds.objExists(asset_group):
+            asset_roots = cmds.listRelatives(asset_group, children=True, fullPath=True)
 
-def publishAssets():
-    start = 0
-    end = 120
-    root = "-root pSphere1 -root pCube1"
-    path_name[] = ["C:/saveFilePath01/", "C:/saveFilePath02/"] #paths are going to be saved in a collection
-    file_name = "alembicTest"
-    extension = ".abc" #will have between Alembic/FBX depending select box value
+            #Alembic Export
+            if (export_type == "alembic"):
+                print("Exporting Alembic...")
+                for asset in asset_roots:
+                    asset_name = asset.split("|")[-1]
+                    export_dir = "{0}/{1}/{2}".format(publish_path, asset_type, asset_name)
+                    try:
+                        os.makedirs(export_dir)
+                    except OSError:
+                        print(export_dir + " already exists")
+                    file_name = "{0}_layout_v{1}.abc".format(asset_name, str(GetNextVersionNumber(publish_path, asset_name, asset_type)).zfill(3))
+                    export_file = export_dir + "/" + file_name
+                    alembic_args = [
+                        '-renderableOnly',
+                        '-file ' + export_file,
+                        '-uvWrite',
+                        '-writeFaceSets',
+                        '-worldSpace',
+                        '-writeVisibility',
+                        '-dataFormat ogawa',
+                        '-root ' + asset,
+                        '-fr %d %d' % (cmds.playbackOptions(q=True, min=True), cmds.playbackOptions(q=True, max=True))
+                    ]
+                    print(alembic_args)
+                    cmds.AbcExport(j = " ".join(alembic_args))
 
-for x in xrange(1,10):
-  
-    #using for loop we are going to extract files to different directories
-    command = "-frameRange " + start + " " + end +" -uvWrite -worldSpace " + root + " -file " + path_name[x]  file_name
-    if extension == ".abc":
-        print("exporting alembix file")
-        cmds.AbcExport (j = command)  
-    if extension == ".fbx":
-        print("exporting FBX file")
-        cmds.FbxExport (j = command)  
+            #FBX Export
+            if (export_type == "fbx"):
+                print("Exporting FBX...")
+                for asset in asset_roots:
+                    asset_name = asset.split("|")[-1]
+                    export_dir = "{0}/{1}/{2}".format(publish_path, asset_type, asset_name)
+                    try:
+                        os.makedirs(export_dir)
+                    except OSError:
+                        print(export_dir + " already exists")
+                    file_name = "{0}_layout_v{1}.abc".format(asset_name, str(GetNextVersionNumber(publish_path, asset_name, asset_type)).zfill(3))
+                    export_file = export_dir + "/" + file_name           
+                    cmds.file(export_file, force=True, options="v=0;", typ="FBX export", pr=True,  ea=True)
 
-# ============================================================
-# UI
-# =============================================================
 
+def GetLatestVersionNumber(path, asset_name, asset_type):
+    dir = "{0}/{1}/{2}".format(path, asset_type,asset_name)
+    found = False
+    count = 1
+    while found == False:
+        if not os.path.isfile(dir + "/{0}_layout_v{1}.abc".format(asset_name, str(count).zfill(3))):
+            found = True
+            count = count - 1
+        else:
+            count = count + 1
+    return count
+
+def GetNextVersionNumber(path, asset_name, asset_type):
+    return GetLatestVersionNumber(path, asset_name, asset_type) + 1
+
+#=======================================          
+#------------------UI-------------------
+#=======================================
+toolName = 'savePublishTool'
+#----------------UI Defs----------------
+def reset_text_fields_background_color():
+    global text_fields
+    print(text_fields)
+    for text_field in text_fields:
+        cmds.text(
+            text_field, 
+            edit=True, 
+            backgroundColor=(0.2667, 0.2667, 0.2667)
+            )
+
+def create_section(section_title, parent):
+    return cmds.frameLayout(label=section_title, collapsable=True, collapse=True, parent=parent, marginWidth=10, marginHeight=10)
+#--------------UI Init------------------
 def createUI():
-    cmds.separator(h=2, backgroundColor = [0.5, 0.5, 0.5], style="single", annotation="buttons", width=cmds.getAttr('defaultResolution.width'))
-    cmds.text(label='Save/Publish Tool', font="boldLabelFont", align='center', width=cmds.getAttr('defaultResolution.width'))
-    cmds.separator(h=2, backgroundColor = [0.5, 0.5, 0.5], style="single", annotation="buttons", width=cmds.getAttr('defaultResolution.width'))
+    if cmds.window(toolName, exists = True):
+        cmds.deleteUI(toolName)
 
-    cmds.button(label = 'Save', command = 'saveAssets()')
-    cmds.button(label = 'Export', command = 'exportAnim()')
+    window_width = 400
+    window_height = 600
 
-    cmds.showWindow('savePublishTool')
+    column1_width = 150 
+    column2_width = 200 
+    column3_width = 250 
 
-def setupTool():
+    color_grey = [0.3, 0.3, 0.3]
+#-----------Window Create---------------
+
+    ic_window = cmds.window(
+        toolName, 
+        title = "Save Publish Tool for Autodesk Maya", 
+        width = window_width, 
+        height = window_height
+        )
+        
+    ic_layout = cmds.columnLayout(adjustableColumn = True) 
+#------------Init Tool Header--------------------
+ 
+    cmds.separator(h=15, style="single", annotation="buttons", width = window_width)
+    cmds.text(label='Asset Save/Publish Tool for Autodesk Maya', backgroundColor = color_grey, font="boldLabelFont", align='center', width = window_width)
+    cmds.text(label='Saves, Publishes and automates file naming of assets', backgroundColor = color_grey, font="smallBoldLabelFont",  align='center', width = window_width)
+    cmds.separator(h=15, style="single", annotation="buttons", width = window_width)
+    
+#------------Init Directory Management-----------
+    create_section("Set Base Directory", ic_window)
+    
+    cmds.rowLayout(numberOfColumns = 2, columnWidth2 = (column1_width, column2_width)) 
+    cmds.text(label="Current Directory:")
+    cmds.text(label="'" + "NULL" + "'")
+    cmds.setParent('..')  # End the rowLayout
+    
+    cmds.rowLayout(numberOfColumns=3, columnWidth3=(column1_width, column2_width, column3_width))
+    cmds.text(label="Set Base Directory: ")
+    cmds.button(label="Configure", command='placeholder()', width = 100)
+    cmds.setParent('..')  # End the rowLayout
+   
+#------------Init Save Assets----------------
+
+    create_section("Save Assets", ic_window)
+    cmds.rowLayout(numberOfColumns = 1, columnWidth1 = column1_width)
+    cmds.textScrollList(
+        isObscured = True,
+        numberOfRows = 10,  
+        allowMultiSelection = True, 
+        width = window_width,
+        height = 300,
+        append = []  
+    )
+    cmds.setParent('..')
+    
+    cmds.rowLayout(numberOfColumns = 2, columnWidth2 = (column1_width, column2_width)) 
+    cmds.text(label="Refresh Asset List: ")
+    cmds.button(label="Refresh List", command='placeholder()', width = 100)
+    cmds.setParent('..')  # End the rowLayout
+    
+    cmds.rowLayout(numberOfColumns=3, columnWidth3=(column1_width, column2_width, column3_width))
+    cmds.text(label="Save displayed assets:")
+    cmds.button(label="Save Assets", command='placeholder()', width = 100)
+    cmds.setParent('..')  # End the rowLayout
+
+#--------Init Publish Assets----------------
+
+    create_section("Publish Assets", ic_window)
+    cmds.rowLayout(numberOfColumns = 1, columnWidth1 = column1_width)
+    cmds.textScrollList(
+        numberOfRows = 10,  
+        allowMultiSelection = True, 
+        width = window_width,
+        height = 300,
+        append = []  
+    )
+    cmds.setParent('..')
+    
+    cmds.rowLayout(numberOfColumns = 2, columnWidth2 = (column1_width, column2_width)) 
+    cmds.text(label="Refresh Asset List: ")
+    cmds.button(label="Refresh List", command='placeholder()', width = 100)
+    cmds.setParent('..')  # End the rowLayout
+
+    cmds.rowLayout(numberOfColumns=3, columnWidth3=(column1_width, column2_width, column3_width))
+    cmds.text(label="Publish Displayed Assets:")
+    cmds.button(label="Publish Assets", command='placeholder()', width = 100)
+    cmds.setParent('..')  # End the rowLayout
+
+#--------------Init Logs-------------------- 
+  
+    create_section("Log Messages", ic_window)
+    cmds.rowLayout(numberOfColumns = 2, columnWidth2 = (column1_width, column2_width))
+    title_label = cmds.text(label="Log Messages")
+    cmds.button(label="Clean Log Messages", command='reset_text_fields_background_color()')
+    cmds.setParent('..')  # End the rowLayout
+    
+    cmds.rowLayout(numberOfColumns = 1, columnWidth1 = column1_width)
+    global scroll_list
+    scroll_list = cmds.textScrollList(
+        numberOfRows=10,  
+        allowMultiSelection=True,  
+        width=window_width,
+        height=300,
+        append=[]
+    )    
+    cmds.setParent('..')  # End the rowLayout
+    cmds.showWindow(ic_window)
+#------------Window Reload------------------
+
+def reloadSavePublishTool():  
+    if cmds.window(toolName, exists = True):
+        cmds.deleteUI(toolName)
     createUI()
-    savePublishTool()
+#------------Show UI Window------------------
+createUI()
